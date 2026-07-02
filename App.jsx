0121e687f3c83488465f9cfef8e66bac81507871
daemanas <daemanas17@gmail.com>
2026-07-03 01:05:50 +0530

    ReactNative Tutorial

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import { View, Text } from 'react-native';
+import React from 'react';
+function App() {
+  return (
+    
+      <View style={{ padding: '20px', backgroundColor: 'red', margin: '30px', alignItems: "center", justifyContent: "center" }}>
+        <Text>Hello World </Text>
+        <Text>i am daem anas</Text>
+      </View>
+   
+  )
+}
+
+export default App;
+
